3be02f9729401c1f7a424f0874552325b3714637
PengBoomOuch <cornea84@gmail.com>
2015-10-23 12:27:50 +0200

    Made into a package

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 Simple bidirectional graph with nodes and connections with costs.
 Comes with A* and Breadth-First Pathfinding, a few heuristics and path smoothing.
 
-Take it, it's yours (GPL-3.0)
+Take it, it's yours (MIT)
+
+For documentation ... plz look at the tests ... for now ...
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graphstar',
+      version='0.1',
+      description='Simple graph with pathfinding',
+      url='https://github.com/pengboomouch/graphstar',
+      author='pengboomouch (Cristian Cornea)',
+      author_email='pengboomouch@gmail.com',
+      license='MIT',
+      packages=['graphstar'],
+      zip_safe=False)
