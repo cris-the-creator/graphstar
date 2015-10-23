66faa7c086ab17b55c6cd5406e46198ac69ae27d
PengBoomOuch <cornea84@gmail.com>
2015-10-23 13:01:55 +0200

    minor errors
    just some mistakes i made

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='graphstar',
       version='0.1',
-      description='Simple graph with pathfinding',
+      description='simple graph with pathfinding',
       url='https://github.com/pengboomouch/graphstar',
-      author='pengboomouch (Cristian Cornea)',
+      author='pengboomouch (cristian cornea)',
       author_email='pengboomouch@gmail.com',
-      license='MIT',
+      license='mit',
       packages=['graphstar'],
       zip_safe=False)
